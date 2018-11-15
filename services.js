14524008f2a6fdfdb3f342cf08704bf4c049d421
Wilson Phan <wphan@pagerduty.com>
2018-11-15 12:27:11 -0800

    Updating AWS payloads and Service Keys

--- services.js
+++ services.js
@@ -1,10 +1,10 @@
 var services = {
 	"01 Ken CloudWatch": "704feb51158e47359ebf872a3ad43aee",
-	"02 Eric CloudWatch": "c226cb147ecb4999b3298968a2f41bfd",
-	"03 Masado CloudWatch": "fc1340b5b1264a43bfe2aaf36c8b023c",
-	"04 Edward CloudWatch": "e3ef0a7d9100408084b8f55bfd118843",
-	"05 John CloudWatch": "05344be7458e465d899d83ac8b1081c0",
-	"06 Cheryl CloudWatch": "1d352ed7b2de42c9ac52dfd9b28b317a",
+	"02 Eric CloudWatch": "61b6d2a5944b453292291dcc34d180e1",
+	"03 Masado CloudWatch": "b2e5f82381b94abd8cc15bdc7519be54",
+	"04 Edward CloudWatch": "67066c29b9234a77aa08ae4ad2402c3c",
+	"05 John CloudWatch": "2a63716a97024493b592952753b60773",
+	"06 Cheryl CloudWatch": "71e65a93e1d74f61b9de5f74c29b1a59",
 	"Martin Test Service AWS CloudWatch": "3db5a969d09647e59c118201730c7935",
 	"Martin Test Service Custom Event Transformer": "f80e23b9e8e546b8ae82dc8a908d6b3a"
 };
