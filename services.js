11070d64df5c80e491c14f5aa9c1c6f35889e4de
Sean Higgins <seanhiggins6@users.noreply.github.com>
2018-11-15 22:23:35 -0800

    More Services
    I added GuardDuty keys so we can send events to the real integrations

--- services.js
+++ services.js
@@ -1,10 +1,18 @@
 var services = {
 	"01 Ken CloudWatch": "704feb51158e47359ebf872a3ad43aee",
+	"01 Ken PHD": "f377979711fa41b593f57284fb6ebce4",
+	"01 Ken GuardDuty": "7d481e115c294f9cbf77c2796137607d",
+	"01 Ken New Relic": "f56a6e39342c42f4bf3dc05015f3123a",
 	"02 Eric CloudWatch": "61b6d2a5944b453292291dcc34d180e1",
+	"02 Eric GuardDuty": "f4c066879faf42e5aba43de9ee9d97ae",
 	"03 Masado CloudWatch": "b2e5f82381b94abd8cc15bdc7519be54",
+	"03 Masado GuardDuty": "e300f6f3f3064464810362446b0d6414",
 	"04 Edward CloudWatch": "67066c29b9234a77aa08ae4ad2402c3c",
+	"04 Edward GuardDuty": "104c29322da74b2d86af634f9294428b",
 	"05 John CloudWatch": "2a63716a97024493b592952753b60773",
+	"05 John GuardDuty": "55f233731f424cb1bb7292578f854092",
 	"06 Cheryl CloudWatch": "71e65a93e1d74f61b9de5f74c29b1a59",
+	"06 Cheryl GuardDuty": "ea0c802e91b24d15b460771bdf210f79",
 	"Martin Test Service AWS CloudWatch": "3db5a969d09647e59c118201730c7935",
 	"Martin Test Service Custom Event Transformer": "f80e23b9e8e546b8ae82dc8a908d6b3a"
 };
